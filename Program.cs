b1f91c3dfab2d460e5f55304ceb3c590ebfae04e
Steak <steak@steakandgravy.com>
2022-12-01 21:47:44 -0500

    Day 1

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Advent2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lstElfCals = new List<int>();
+            var currentElf = 0;
+            // Read each line of the file into a string array. Each element
+                // of the array is one line of the file.
+            string[] lines = System.IO.File.ReadAllLines(@"input");
+            foreach (string line in lines)
+            {
+                if (line.Length > 0)
+                {
+                    currentElf += Int32.Parse(line);
+                }
+                else
+                {
+                    lstElfCals.Add(currentElf);
+                    currentElf = 0;
+                }
+            }
+            lstElfCals.Sort();
+            lstElfCals.Reverse();
+            
+            Console.WriteLine(lstElfCals[0]+lstElfCals[1]+lstElfCals[2]);
+        }
+    }
+}
